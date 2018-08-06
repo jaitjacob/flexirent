12d8bc6f2e049f68c9dceacd801774bbbac49637
jaitjacob <jai8.jacob@gmail.com>
2018-08-07 00:55:05 +1000

    ALL CLEAR! Start again!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Main. I'm just the party starter. From here on FlexiRentSystem will be taking over. Thank you.");
 
 
-        FlexiRentSystem start = new FlexiRentSystem();
-        start.start();
+
+
+
+
+        
 
     }
 }
--- src/com/company/RentalRecord.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class RentalRecord extends  FlexiRentSystem{
-
-
-
-
-
-}
