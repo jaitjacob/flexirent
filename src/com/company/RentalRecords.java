e474e65880af1cbee20beb35ccc458b2c33fcdbe
jaitjacob <jai8.jacob@gmail.com>
2018-08-08 14:54:05 +1000

    1.Added the helper class provided which is DateTime.java 2. over rode the toString() method that returns a string in the specified format asked in the specification.

--- src/com/company/RentalRecords.java
+++ src/com/company/RentalRecords.java
@@ -2,12 +2,38 @@ package com.company;
 
 public class RentalRecords {
 
-    String recordId;
+    String recordId;//propertyId_ + customerId_ + rentDate (8 digit format: ddmmyyyy)
     DateTime rentDate;
-    String
-    String
-    String
-    String
+    DateTime estimatedReturnDate;
+    DateTime actualReturnDate = new DateTime(0,0,0);
+    double rentalFee;
+    double lateFee;
+
+    //constructor
+
+
+
+    //Override the public String toString() method to return a string containing the details of a rental record in the following format.
+    //recordId:rentDate:estimatedReturnDate:actualReturnDate:rentalFee:lateFee
+    public String toString(){
+        String returnString="";
+        DateTime noneDate = new DateTime(0,0,0);
+
+        returnString += recordId+":"+rentDate.getFormattedDate()+":"+estimatedReturnDate.getFormattedDate()+":";
+
+        int diff = DateTime.diffDays(noneDate,actualReturnDate);
+        if(diff==0){
+            returnString+="none:none:none";
+            return returnString;
+        }
+        else{
+            returnString+=""+actualReturnDate.getFormattedDate()+":"+rentalFee+":"+lateFee;
+            return returnString;
+        }
+    }
+
+
+
 
 
 
